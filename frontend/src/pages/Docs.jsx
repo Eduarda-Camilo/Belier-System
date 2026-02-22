84aa4c671b6441852d3d2257b76aad6511931672
Eduarda-Camilo <eduarda.camilo@aluno.ufop.edu.br>
2026-02-22 12:05:19 -0300

    Update Docs.jsx

--- frontend/src/pages/Docs.jsx
+++ frontend/src/pages/Docs.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useComponents } from '../contexts/ComponentsContext';
 import './Docs.css';
